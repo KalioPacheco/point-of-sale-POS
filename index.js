28564012fc828bf688cf87d988203283a4c18836
aleulc <ooooo.alc61@gmail.com>
2026-03-12 16:42:02 -0600

    MVPX-feature-CRUDCategoria-86dza8py8

--- index.js
+++ index.js
@@ -33,7 +33,7 @@ app.use(passport.session());
 app.use((req, res, next) => {
   res.header('Access-Control-Allow-Origin', '*');
   res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization, idempotency-key');
-  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, PATCH, DELETE, OPTIONS');
   next();
 });
 
